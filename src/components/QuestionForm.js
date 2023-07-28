930da5095b648100de504a7b50978eb8380703b5
GASgod1111 <reagan.peter@student.moringaschool.com>
2023-07-28 12:57:42 +0300

    done

--- src/components/QuestionForm.js
+++ src/components/QuestionForm.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-function QuestionForm(props) {
+function QuestionForm({ setQuestions }) {
   const [formData, setFormData] = useState({
     prompt: "",
     answer1: "",
@@ -19,14 +19,34 @@ function QuestionForm(props) {
 
   function handleSubmit(event) {
     event.preventDefault();
-    console.log(formData);
+    // Create the new question object
+    const newQuestion = {
+      prompt: formData.prompt,
+      answers: [formData.answer1, formData.answer2, formData.answer3, formData.answer4],
+      correctIndex: parseInt(formData.correctIndex), // Convert to integer
+    };
+
+    // Send a POST request to create a new question
+    fetch('http://localhost:4000/questions', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newQuestion),
+    })
+      .then(response => response.json())
+      .then(data => {
+        // Update the state with the new question
+        setQuestions(prevQuestions => [...prevQuestions, data]);
+      })
+      .catch(error => console.error('Error creating question:', error));
   }
 
   return (
     <section>
       <h1>New Question</h1>
       <form onSubmit={handleSubmit}>
-        <label>
+      <label>
           Prompt:
           <input
             type="text"
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,12 +1,68 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 
-function QuestionList() {
+const QuestionList = ({ questions, setQuestions }) => {
+  const handleDelete = (id) => {
+
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: 'DELETE',
+    })
+      .then(() => {
+        setQuestions((prevQuestions) =>
+          prevQuestions.filter((question) => question.id !== id)
+        );
+      })
+      .catch((error) => console.error('Error deleting question:', error));
+  };
+
+  const handleCorrectIndexChange = (id, correctIndex) => {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ correctIndex }),
+    })
+      .then(() => {
+    
+        setQuestions((prevQuestions) =>
+          prevQuestions.map((question) =>
+            question.id === id ? { ...question, correctIndex } : question
+          )
+        );
+      })
+      .catch((error) => console.error('Error updating question:', error));
+  };
+
   return (
-    <section>
-      <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
-    </section>
+    <div>
+      <h2>Question List</h2>
+      <ul>
+        {questions.map((question) => (
+          <li key={question.id}>
+            <p>{question.prompt}</p>
+            <ul>
+              {question.answers.map((answer, index) => (
+                <li key={index}>{answer}</li>
+              ))}
+            </ul>
+            <select
+              value={question.correctIndex}
+              onChange={(event) =>
+                handleCorrectIndexChange(question.id, event.target.value)
+              }
+            >
+              {question.answers.map((_, index) => (
+                <option key={index} value={index}>
+                  {index}
+                </option>
+              ))}
+            </select>
+            <button onClick={() => handleDelete(question.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
-}
+};
 
 export default QuestionList;
